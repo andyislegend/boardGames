0be430d4c3ebd0172611d8062fbd2c9e1f067bdb
tarasVarvariuk <taras20109@gmail.com>
2016-04-11 20:06:49 +0300

    added main page sample

---
+++ web/src/main/webapp/resources/js/gamesApp.js
@@ -0,0 +1,23 @@
+var app = angular.module('boardGamesApp', []);
+app.controller('getGamesGlobalController', function($scope) {
+    $scope.gamesGlobal = [
+        {
+            Name:"Chess", Category:"Strategic", 
+            Description:"Most popular game ever",
+            MinPlayers:"2", MaxPlayers:"2"
+        },
+        {
+            Name:"Monopolly", Category:"Buisiness",
+            Description:"Some awesome things about this game",
+            MinPlayers:"3", MaxPlayers:"6"
+        }
+    ];
+});
+app.controller('getGamesMyController', function($scope) {
+    $scope.gamesMy = [
+        {Name:"Dangeons of dragons"},
+        {Name:"Game of thrones"},
+        {Name:"Chess"},
+        {Name:"Awesome dudes"}
+    ];
+});
