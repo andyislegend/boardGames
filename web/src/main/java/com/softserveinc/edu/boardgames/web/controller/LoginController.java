7d3fcd7c1478c88e21675bda73816a079a47e08e
andyislegend <andrew.petryk@gmail.com>
2016-04-12 11:08:49 +0300

    additional reowrk to security-context

--- web/src/main/java/com/softserveinc/edu/boardgames/web/controller/LoginController.java
+++ web/src/main/java/com/softserveinc/edu/boardgames/web/controller/LoginController.java
@@ -12,12 +12,14 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 @Controller
 public class LoginController {
 	
 	 @RequestMapping(value = "/login", method = RequestMethod.GET)
 	    public String loginPage() {
-	        return "login";
+	        
+		 return "login";
 	    }
 	
 	 @RequestMapping(value="/logout", method = RequestMethod.GET)
