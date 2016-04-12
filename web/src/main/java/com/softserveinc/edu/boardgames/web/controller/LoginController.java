c8703f188ecb6dd2fb9bfd2d022d477ccfeac027
andyislegend <andrew.petryk@gmail.com>
2016-04-12 10:41:13 +0300

    slight changes to loginController

--- web/src/main/java/com/softserveinc/edu/boardgames/web/controller/LoginController.java
+++ web/src/main/java/com/softserveinc/edu/boardgames/web/controller/LoginController.java
@@ -15,25 +15,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LoginController {
 	
-//	@RequestMapping(value = "/login", method = RequestMethod.GET)
-//	public ModelAndView login(
-//		@RequestParam(value = "error", required = false) String error,
-//		@RequestParam(value = "logout", required = false) String logout) {
-//
-//		ModelAndView model = new ModelAndView();
-//		if (error != null) {
-//			model.addObject("error", "Invalid username and password!");
-//		}
-//
-//		if (logout != null) {
-//			model.addObject("msg", "You've been logged out successfully.");
-//		}
-//		model.setViewName("login");
-//
-//		return model;
-//
-//	}
-	
 	 @RequestMapping(value = "/login", method = RequestMethod.GET)
 	    public String loginPage() {
 	        return "login";
