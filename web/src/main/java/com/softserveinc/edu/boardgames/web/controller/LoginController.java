71d65f3ab5fa0d18fa6153471acd3de5c9da8846
andyislegend <andrew.petryk@gmail.com>
2016-04-12 10:35:59 +0300

    better version of wirking spring-security auth

--- web/src/main/java/com/softserveinc/edu/boardgames/web/controller/LoginController.java
+++ web/src/main/java/com/softserveinc/edu/boardgames/web/controller/LoginController.java
@@ -5,34 +5,39 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
 	
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public ModelAndView login(
-		@RequestParam(value = "error", required = false) String error,
-		@RequestParam(value = "logout", required = false) String logout) {
-
-		ModelAndView model = new ModelAndView();
-		if (error != null) {
-			model.addObject("error", "Invalid username and password!");
-		}
-
-		if (logout != null) {
-			model.addObject("msg", "You've been logged out successfully.");
-		}
-		model.setViewName("login");
-
-		return model;
-
-	}
+//	@RequestMapping(value = "/login", method = RequestMethod.GET)
+//	public ModelAndView login(
+//		@RequestParam(value = "error", required = false) String error,
+//		@RequestParam(value = "logout", required = false) String logout) {
+//
+//		ModelAndView model = new ModelAndView();
+//		if (error != null) {
+//			model.addObject("error", "Invalid username and password!");
+//		}
+//
+//		if (logout != null) {
+//			model.addObject("msg", "You've been logged out successfully.");
+//		}
+//		model.setViewName("login");
+//
+//		return model;
+//
+//	}
+	
+	 @RequestMapping(value = "/login", method = RequestMethod.GET)
+	    public String loginPage() {
+	        return "login";
+	    }
 	
 	 @RequestMapping(value="/logout", method = RequestMethod.GET)
 	    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
@@ -41,5 +46,23 @@ public class LoginController {
 	            new SecurityContextLogoutHandler().logout(request, response, auth);
 	        }
 	        return "redirect:/login?logout";
+	    }
+	 
+	 @RequestMapping(value = "/access_denied", method = RequestMethod.GET)
+	    public String accessDeniedPage(ModelMap model) {
+	        model.addAttribute("user", getPrincipal());
+	        return "accessDenied";
+	    }
+	 
+	 private String getPrincipal(){
+	        String userName = null;
+	        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+	 
+	        if (principal instanceof UserDetails) {
+	            userName = ((UserDetails)principal).getUsername();
+	        } else {
+	            userName = principal.toString();
+	        }
+	        return userName;
 	    }
 }
