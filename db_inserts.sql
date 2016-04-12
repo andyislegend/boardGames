172c98c9c4b8d4e5a6bd3e4f3a82fe08b909f4cf
DoraBond <dasbu34@gmail.com>
2016-04-12 13:40:26 +0300

    create sql script file for inserts to database

---
+++ db_inserts.sql
@@ -0,0 +1,29 @@
+
+use boardgames;
+-- here inserts for all tables
+
+--  inserts to table roles
+
+
+
+--  inserts to table category
+
+
+
+--  inserts to table address
+
+
+
+--  inserts to table user
+
+
+
+--  inserts to table game
+
+
+
+--  inserts to table friends
+
+
+
+--  inserts to table user_game
