a8912e9cb5c48e093c8be7df8eb4b1ec32f06428
andyislegend <andrew.petryk@gmail.com>
2016-04-17 00:37:41 +0300

    add registration.jsp, add register controller, proceed with validation(not yet done)

--- web/src/main/java/com/softserveinc/edu/boardgames/web/controller/RegisterController.java
+++ web/src/main/java/com/softserveinc/edu/boardgames/web/controller/RegisterController.java
@@ -1,5 +1,68 @@
 package com.softserveinc.edu.boardgames.web.controller;
 
+import java.util.Locale;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.softserveinc.edu.boardgames.persistence.entity.User;
+import com.softserveinc.edu.boardgames.service.UserService;
+
+//@Controller
 public class RegisterController {
+
+//	@Autowired
+//	UserService userService;
+//	
+//	@Autowired
+//    MessageSource messageSource;
+//
+//	@RequestMapping(value = { "/newuser" }, method = RequestMethod.GET)
+//	public String newUser(ModelMap model) {
+//		User user = new User();
+//		model.addAttribute("user", user);
+//		model.addAttribute("edit", false);
+//		return "registration";
+//	}
+//
+//	@RequestMapping(value = { "/newuser" }, method = RequestMethod.POST)
+//	public String saveUser(@Valid User user, BindingResult result, ModelMap model) {
+//
+//		if (result.hasErrors()) {
+//			return "registration";
+//		}
+//
+//		if (!userService.isExistsWithUsername(user.getUsername())) {
+//			FieldError ssoError = new FieldError("user", "ssoId", messageSource.getMessage("non.unique.ssoId",
+//					new String[] { user.getUsername() }, Locale.getDefault()));
+//			result.addError(ssoError);
+//			return "registration";
+//		}
+//
+//		userService.createUser(user);;
+//
+//		model.addAttribute("success",
+//				"User " + user.getFirstName() + " " + user.getLastName() + " registered successfully");
+//		// return "success";
+//		return "registrationsuccess";
+//	}
+//
+//	
+//	@RequestMapping(value = { "/edit-user-{ssoId}" }, method = RequestMethod.GET)
+//	public String editUser(@PathVariable String username, ModelMap model) {
+//		User user = userService.getUser(username);
+//		model.addAttribute("user", user);
+//		model.addAttribute("edit", true);
+//		return "registration";
+//	}
 
 }
