273301a8f58a99d48b4770db7b85d0cc8dff7460
andyislegend <andrew.petryk@gmail.com>
2016-04-18 17:33:04 +0300

    fixed sql script

--- db_inserts.sql
+++ db_inserts.sql
@@ -1,18 +1,25 @@
 
 use boardgames;
 
+--  inserts to table address
+INSERT INTO `boardgames`.`address` (`city`, `country`, `houseNumber`, `postCode`, `roomNumber`, `street`) VALUES ('Lviv', 'Ukraine', '100', '79002', '23', 'pr. Svobody');
+INSERT INTO `boardgames`.`address` (`city`, `country`, `houseNumber`, `postCode`, `roomNumber`, `street`) VALUES ('Kyiv', 'Ukraine', '56', '86005', '78', 'Checheta');
+INSERT INTO `boardgames`.`address` (`city`, `country`, `houseNumber`, `postCode`, `roomNumber`, `street`) VALUES ('Donetsk', 'Ukraine', '12', '77445', '25', 'Ozerna');
+INSERT INTO `boardgames`.`address` (`city`, `country`, `houseNumber`, `postCode`, `roomNumber`, `street`) VALUES ('Odessa', 'Ukraine', '86', '98765', '49', 'Shyroka');
+
+
 --  inserts to table user
 insert into users (age, email, firstName, lastName, password, phoneNumber, rating, sex, state, username, addressId) values
-(99, 'root@root.com', 'Super', 'Admin', '$2a$10$e2qEa0wunoicRAGky9Kd7O..A5YpXbUy3TDMhtrQ3FG3tkYsOpPmW', '+380000000000', 'GODLIKE', 'none', 'ACTIVE', 'root', '1');
+(99, 'root@root.com', 'Super', 'Admin', '$2a$10$e2qEa0wunoicRAGky9Kd7O..A5YpXbUy3TDMhtrQ3FG3tkYsOpPmW', '+380000000000', 'GODLIKE', 'none', 'ACTIVE', 'root', 1);
 
 insert into users (age, email, firstName, lastName, password, phoneNumber, rating, sex, state, username, addressId) values
-(25, 'kravets@gmail.com', 'Artem', 'Kravets', '$2a$10$e2qEa0wunoicRAGky9Kd7O..A5YpXbUy3TDMhtrQ3FG3tkYsOpPmW', '+380957987654', 'NOOB', 'male', 'ACTIVE', 'kravets', '2');
+(25, 'kravets@gmail.com', 'Artem', 'Kravets', '$2a$10$e2qEa0wunoicRAGky9Kd7O..A5YpXbUy3TDMhtrQ3FG3tkYsOpPmW', '+380957987654', 'NOOB', 'male', 'ACTIVE', 'kravets', 2);
 
 insert into users (age, email, firstName, lastName, password, phoneNumber, rating, sex, state, username, addressId) values
-(37, 'bondar@gmail.com', 'Ivan', 'Bondar', '$2a$10$e2qEa0wunoicRAGky9Kd7O..A5YpXbUy3TDMhtrQ3FG3tkYsOpPmW', '+380957987654', 'NOOB', 'male', 'ACTIVE', 'bondar', '3');
+(37, 'bondar@gmail.com', 'Ivan', 'Bondar', '$2a$10$e2qEa0wunoicRAGky9Kd7O..A5YpXbUy3TDMhtrQ3FG3tkYsOpPmW', '+380957987654', 'NOOB', 'male', 'ACTIVE', 'bondar', 3);
 
 insert into users (age, email, firstName, lastName, password, phoneNumber, rating, sex, state, username, addressId) values
-(37, 'khariv@gmail.com', 'Body', 'Khariv', '$2a$10$e2qEa0wunoicRAGky9Kd7O..A5YpXbUy3TDMhtrQ3FG3tkYsOpPmW', '+380957987654', 'NOOB', 'male', 'ACTIVE', 'khariv', '3');
+(37, 'khariv@gmail.com', 'Body', 'Khariv', '$2a$10$e2qEa0wunoicRAGky9Kd7O..A5YpXbUy3TDMhtrQ3FG3tkYsOpPmW', '+380957987654', 'NOOB', 'male', 'ACTIVE', 'khariv', 3);
 
 -- inserts into user_role table
 insert into user_role (username, value) values
@@ -28,16 +35,10 @@ insert into category (name) values ('children games');
 insert into category (name) values ('logical');
 
 
---  inserts to table address
-INSERT INTO `boardgames`.`address` (`city`, `country`, `houseNumber`, `postCode`, `roomNumber`, `street`) VALUES ('Lviv', 'Ukraine', '100', '79002', '23', 'pr. Svobody');
-INSERT INTO `boardgames`.`address` (`city`, `country`, `houseNumber`, `postCode`, `roomNumber`, `street`) VALUES ('Kyiv', 'Ukraine', '56', '86005', '78', 'Checheta');
-INSERT INTO `boardgames`.`address` (`city`, `country`, `houseNumber`, `postCode`, `roomNumber`, `street`) VALUES ('Donetsk', 'Ukraine', '12', '77445', '25', 'Ozerna');
-INSERT INTO `boardgames`.`address` (`city`, `country`, `houseNumber`, `postCode`, `roomNumber`, `street`) VALUES ('Odessa', 'Ukraine', '86', '98765', '49', 'Shyroka');
-
 --  inserts to table game
-insert into game (name,description,minPlayers,maxPlayers,categoryId,rating) values ('Monopoly','Bla la description',
+insert into game (name,description,minPlayers,maxPlayers,categoryId, gameRating) values ('Monopoly','Bla la description',
 2,4,1,0.0);
-insert into game (name,description,minPlayers,maxPlayers,categoryId,rating) values ('MEMO','Bla la description',
+insert into game (name,description,minPlayers,maxPlayers,categoryId,gameRating) values ('MEMO','Bla la description',
 2,4,2,0.0);
 
 --  inserts to table status
