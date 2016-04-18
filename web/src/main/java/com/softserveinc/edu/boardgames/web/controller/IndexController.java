edae1b425dafce18cf3cfb43b5ae66e3e9454004
andyislegend <andrew.petryk@gmail.com>
2016-04-18 13:46:17 +0300

    create WebUtil class for get Principal (username) on page

--- web/src/main/java/com/softserveinc/edu/boardgames/web/controller/IndexController.java
+++ web/src/main/java/com/softserveinc/edu/boardgames/web/controller/IndexController.java
@@ -1,11 +1,11 @@
 package com.softserveinc.edu.boardgames.web.controller;
 
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.softserveinc.edu.boardgames.web.util.WebUtil;
 
 @Controller
 public class IndexController {
@@ -17,21 +17,9 @@ public class IndexController {
 	
 	@RequestMapping(value = "/home**", method = RequestMethod.GET)
 	public final String getUserPage(Model model){
-		model.addAttribute("user", getPrincipal());
+		model.addAttribute("user", WebUtil.getPrincipalUsername());
 		
 		return "home";
 	}
 	
-	
-	private String getPrincipal(){
-        String userName = null;
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
- 
-        if (principal instanceof UserDetails) {
-            userName = ((UserDetails)principal).getUsername();
-        } else {
-            userName = principal.toString();
-        }
-        return userName;
-    }
 }
