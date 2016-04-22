02d4f6eef261d052e44d3a26efdb1d0eeae3b43d
Volodymyr95 <krokhmalyuk@live.com>
2016-04-23 01:48:18 +0300

    Delete unused code

--- web/src/main/java/com/softserveinc/edu/boardgames/web/controller/CurrentUserGameController.java
+++ web/src/main/java/com/softserveinc/edu/boardgames/web/controller/CurrentUserGameController.java
@@ -1,25 +1,20 @@
 package com.softserveinc.edu.boardgames.web.controller;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.softserveinc.edu.boardgames.persistence.entity.Game;
+import com.softserveinc.edu.boardgames.persistence.entity.Category;
 import com.softserveinc.edu.boardgames.persistence.entity.GameUser;
-import com.softserveinc.edu.boardgames.persistence.entity.User;
 import com.softserveinc.edu.boardgames.persistence.entity.dto.GameUserDTO;
-import com.softserveinc.edu.boardgames.persistence.repository.GameRepository;
-import com.softserveinc.edu.boardgames.persistence.repository.GameUserRepository;
-import com.softserveinc.edu.boardgames.persistence.repository.UserRepository;
+import com.softserveinc.edu.boardgames.service.CategoryService;
 import com.softserveinc.edu.boardgames.service.GameUserService;
 import com.softserveinc.edu.boardgames.service.UserService;
 import com.softserveinc.edu.boardgames.service.mapper.GameUserMapper;
@@ -37,6 +32,9 @@ public class CurrentUserGameController {
 	private UserService userService;
 
 	@Autowired
+	private CategoryService categoryService;
+	
+	@Autowired
 	private GameUserService gameUserService;
 	
 	@RequestMapping(value = "/getAllGamesCurUser", method = RequestMethod.GET)
@@ -51,10 +49,17 @@ public class CurrentUserGameController {
 		return gameUserDTOs;
 	}
 	
-	@RequestMapping(value = "NewGame", method = RequestMethod.POST)
-	public String addNewGame(@RequestBody GameUser gameUser){		 
+	@RequestMapping(value = "NewGame", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void addNewGame(@RequestBody GameUser gameUser){		 
 		gameUser.setUser(userService.getUser(WebUtil.getPrincipalUsername()));
 		gameUserService.create(gameUser);
-		return "";
+		
+	}
+	
+	@RequestMapping(value = "/getAllCategories", method = RequestMethod.GET)
+	@ResponseBody
+	public List<Category> allCategories (){
+		List<Category> categories = categoryService.getAll();
+		return categories;
 	}
 }
