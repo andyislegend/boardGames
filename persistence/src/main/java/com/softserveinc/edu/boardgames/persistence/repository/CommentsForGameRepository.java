ec7ac96240614a91cbbc2909569f5ea3658a46bc
DoraBond <dasbu34@gmail.com>
2016-04-23 13:02:18 +0300

    Fixed conflict with entity

--- persistence/src/main/java/com/softserveinc/edu/boardgames/persistence/repository/CommentsForGameRepository.java
+++ persistence/src/main/java/com/softserveinc/edu/boardgames/persistence/repository/CommentsForGameRepository.java
@@ -1,11 +1,12 @@
 package com.softserveinc.edu.boardgames.persistence.repository;
 
+import com.softserveinc.edu.boardgames.persistence.entity.CommentsForGame;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Transactional
-public interface CommentsForGameRepository extends JpaRepository<CommentsForGameRepository, Integer> {
+public interface CommentsForGameRepository extends JpaRepository<CommentsForGame, Integer> {
 
 }
