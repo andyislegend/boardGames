b4b1ba6a710304fb1f0f1f6bec3ce0ece789be9c
DoraBond <dasbu34@gmail.com>
2016-04-23 12:53:44 +0300

    Fixed entity name conflict

--- persistence/src/main/java/com/softserveinc/edu/boardgames/persistence/repository/CommentsForGameRepository.java
+++ persistence/src/main/java/com/softserveinc/edu/boardgames/persistence/repository/CommentsForGameRepository.java
@@ -4,10 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.softserveinc.edu.boardgames.persistence.entity.ÑommentsForGame;
-
 @Repository
 @Transactional
-public interface CommentsForGameRepository extends JpaRepository<ÑommentsForGame, Integer> {
+public interface CommentsForGameRepository extends JpaRepository<CommentsForGameRepository, Integer> {
 
 }
