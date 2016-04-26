d6f5b9a6188b413fe2de112b441bbe9250bdf050
andyislegend <andrew.petryk@gmail.com>
2016-04-26 15:34:10 +0300

    add javadoc ro mail service

--- service/src/main/java/com/softserveinc/edu/boardgames/service/configuration/CustomUserDetailsService.java
+++ service/src/main/java/com/softserveinc/edu/boardgames/service/configuration/CustomUserDetailsService.java
@@ -16,30 +16,43 @@ import com.softserveinc.edu.boardgames.persistence.entity.User;
 import com.softserveinc.edu.boardgames.persistence.enumeration.UserRoles;
 import com.softserveinc.edu.boardgames.service.UserService;
 
+/**
+ * Used for provide UserDetailsService to SprinSecurityConfiguration
+ * 
+ * @author Andrii Petryk
+ *
+ */
 @Service("customUserDetailsService")
-public class CustomUserDetailsService implements UserDetailsService{
+public class CustomUserDetailsService implements UserDetailsService {
 
 	@Autowired
-    private UserService userService;
-	
-	@Transactional(readOnly=true)
-    public UserDetails loadUserByUsername(String username)
-            throws UsernameNotFoundException {
-        User user = userService.getUser(username);
-        if(user==null){
-            throw new UsernameNotFoundException("Username not found"); 
-        }
-            return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), 
-                 user.getState().equals("ACTIVE"), true, true, true, getGrantedAuthorities(user));
-    }
-	
-	private List<GrantedAuthority> getGrantedAuthorities(User user){
-        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-         
-        for(UserRoles userRoles : user.getUserRoles()){
-            authorities.add(new SimpleGrantedAuthority("ROLE_"+userRoles.toString()));
-        }
-        System.out.print("authorities :"+authorities);
-        return authorities;
-    }
+	private UserService userService;
+
+	/**
+	 * Load USerDetails into security
+	 * 
+	 * @param usernmae
+	 *            is used for searching a user and providing its password, role,
+	 *            state
+	 * 
+	 */
+	@Transactional(readOnly = true)
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userService.getUser(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("Username not found");
+		}
+		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+				user.getState().equals("ACTIVE"), true, true, true, getGrantedAuthorities(user));
+	}
+
+	private List<GrantedAuthority> getGrantedAuthorities(User user) {
+		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+
+		for (UserRoles userRoles : user.getUserRoles()) {
+			authorities.add(new SimpleGrantedAuthority("ROLE_" + userRoles.toString()));
+		}
+		System.out.print("authorities :" + authorities);
+		return authorities;
+	}
 }
