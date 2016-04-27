05ebe0409d48c77dbf3ee8d2d603a8ae84db5e6b
VasulBera <vasulbera@gmail.com>
2016-04-28 02:23:46 +0300

    add javadoc

--- persistence/src/main/java/com/softserveinc/edu/boardgames/persistence/repository/FriendRepository.java
+++ persistence/src/main/java/com/softserveinc/edu/boardgames/persistence/repository/FriendRepository.java
@@ -1,41 +1,41 @@
 package com.softserveinc.edu.boardgames.persistence.repository;
 
-import java.util.List;
-import java.util.Optional;
-
-import javax.persistence.NamedNativeQuery;
-import javax.persistence.NamedQuery;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import com.softserveinc.edu.boardgames.persistence.entity.Friend;
 import com.softserveinc.edu.boardgames.persistence.entity.User;
+
+/**
+ * This interface has method for work with friends
+ * 
+ * @author Vasyl Bervetskyy
+ * 
+ */
 
 @Repository
 public interface FriendRepository extends JpaRepository<Friend, Long> {
 		
-		
-		
+		/**
+		 * This method for finding all friend request
+		 */
 		@Query("SELECT COUNT(f) FROM Friend f WHERE f.userId.username = ?1 AND f.status.id = 1")
 		public Integer findCountNoConsiderFrinds(String username);
 		
-				
+		/**
+		 * This method for changing status of friendship to accepted
+		 */	
 		@Modifying
 		@Query("UPDATE Friend f SET f.status.id = 2 WHERE f.user = ?1 AND f.userId = ?2")
 		public void changeStatusOfFriendshipToAccepted(User user, User userId);
 		
-		/*@Modifying
-		@Query("UPDATE Friend f SET f.status.id = 2 WHERE f.user.username = ?1 AND f.userId.username = ?2")
-		public void changeStatusOfFriendshipToAccepted(String curretUserName, String friendsUserName);*/
-		
+		/**
+		 * This method for changing status of friendship to rejected
+		 */	
 		@Modifying
 		@Query("UPDATE Friend f SET f.status.id = 3 WHERE f.user = ?1 AND f.userId = ?2")
 		public void changeStatusOfFriendshipToRejected(User user, User userId);
-		
-		/*@Modifying
-		@Query("UPDATE Friend f SET f.status.id = 3 WHERE f.user.username = ?1 AND f.userId.username = ?2")
-		public void changeStatusOfFriendshipToRejected(String curretUserName, String friendsUserName);*/
 		
 }
--- persistence/src/main/java/com/softserveinc/edu/boardgames/persistence/repository/UserRepository.java
+++ persistence/src/main/java/com/softserveinc/edu/boardgames/persistence/repository/UserRepository.java
@@ -38,6 +38,18 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	@Query("Select u.username FROM User u WHERE u.userRating <=-5")
 	public List<String> findUsesrWithNegativeRating();
 	
+	/**
+	 * This method for finding all users wich is not your friends or 
+	 * users who you sent request to be friends.
+	 * You can find user by name and last name or at first last name and name, it doesn't matter
+	 * 
+	 * @author Vasyl Bervetskyy
+	 * 
+	 * @param userName it's your name
+	 * @param name it's name of user that you are finding 
+	 * @param last name it's last name of user that you are finding 
+	 * @return list of users
+	 */	
 	@Query("SELECT u FROM User u WHERE u.username != ?1 "
 			+ "AND u NOT IN ("
 			+ "SELECT u FROM Friend f RIGHT JOIN f.userId u WHERE f.user.username = ?1 AND (f.status.id = 2 OR f.status.id = 1))"
@@ -46,9 +58,26 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 			+ "AND ((u.firstName LIKE ?2 AND u.lastName LIKE ?3) OR (u.firstName LIKE ?3 AND u.lastName LIKE ?2))")
 	public List<User> findAllUserByFirstNameAndLastName(String userName, String name, String lastName);
 	
+	/**
+	 * This method for finding all your friends
+	 * 
+	 * @author Vasyl Bervetskyy
+	 * 
+	 * @param userName
+	 * @return list of users
+	 */	
 	@Query("SELECT u FROM Friend f RIGHT JOIN f.userId u WHERE f.user.username = ?1 AND f.status.id = 2")
 	public List<User> findAllFriends(String userName);
 	
+	/**
+	 * This method for finding your all not consider friends.
+	 * This users want to be your friends, and they sent you request to be them.
+	 * 
+	 * @author Vasyl Bervetskyy
+	 * 
+	 * @param userName
+	 * @return list of users
+	 */	
 	@Query("SELECT u FROM Friend f RIGHT JOIN f.user u WHERE f.userId.username = ?1 AND f.status.id = 1")
 	public List<User> getAllNoConsiderFriendByUser(String userName);
 	
