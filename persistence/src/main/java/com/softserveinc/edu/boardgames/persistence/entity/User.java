b34da68d4f34b5a86572dc274ad25a78070c756d
vterlyha <vterlyha@gmail.com>
2016-05-13 21:17:42 +0300

    Minor code changes

--- persistence/src/main/java/com/softserveinc/edu/boardgames/persistence/entity/User.java
+++ persistence/src/main/java/com/softserveinc/edu/boardgames/persistence/entity/User.java
@@ -135,8 +135,8 @@ public class User implements Serializable {
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "user", fetch = FetchType.LAZY)
 	private Set<GameUser> userGames;
 
-/*	@OneToMany(cascade = CascadeType.ALL, mappedBy = "user", fetch = FetchType.LAZY)
-	private Set<Exchange> exchanges;*/
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "user", fetch = FetchType.LAZY)
+	private Set<Exchange> exchanges;
 	
 	/**
 	 * Describes address where user lives. Has a many to one relationship to
