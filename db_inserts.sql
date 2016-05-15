b0dc8427cb255057a96009ac36be57416ad6879e
Volodymyr95 <krokhmalyuk@live.com>
2016-05-15 23:20:33 +0300

    delete tournaments insert

--- db_inserts.sql
+++ db_inserts.sql
@@ -204,12 +204,3 @@ INSERT INTO `boardgames`.`events` (`name`, `description`, `imgSrc`, `date`, `pla
 INSERT INTO `boardgames`.`events` (`name`, `description`, `imgSrc`, `date`, `place`, `userId`, `gameId`) VALUES ('Loving and Linkin', 'Dominos event', 'resources/images/dominosImg.jpg', '2016-07-13', 'Lviv', '3', '9');
 INSERT INTO `boardgames`.`events` (`name`, `description`, `imgSrc`, `date`, `place`, `userId`, `gameId`) VALUES ('When your body is the canvas', 'Pictionary event', 'resources/images/pictionaryImg.jpg', '2016-07-22', 'Lviv', '2', '7');
 INSERT INTO `boardgames`.`events` (`name`, `description`, `imgSrc`, `date`, `place`, `userId`, `gameId`) VALUES ('This is simply a wonderful visual feast', 'Utopia event', 'resources/images/utopiaImg.jpg', '2016-08-20', 'Lviv', '1', '8');
-
---  inserts to table tournament
-insert into tournament (name,userCreator_id,game_id,requiredRating,dateOfTournament,maxParticipants,city,country,addition) values ('MonopolyTournament',1,1,2.5,'2016-04-12',3,'Ukraine','Lviv','Join with us');
-insert into tournament (name,userCreator_id,game_id,requiredRating,dateOfTournament,maxParticipants,city,country,addition) values ('MEMOTournament',2,2,3.8,'2016-05-18',4,'Ukraine','Lviv','Will be fun');
-
---  inserts to table tournament_composition
-insert into tournament_composition (tournament_id,userGuest_id) values (1,2);
-insert into tournament_composition (tournament_id,userGuest_id) values (1,3);
-insert into tournament_composition (tournament_id,userGuest_id) values (2,4);
