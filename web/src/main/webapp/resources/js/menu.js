8c4c40aec463d02f2c8a0635dfb6ecfa16dccc4b
andyislegend <andrew.petryk@gmail.com>
2016-05-15 23:29:27 +0300

    first part of editing home page styles

--- web/src/main/webapp/resources/js/menu.js
+++ web/src/main/webapp/resources/js/menu.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
-    var menu = 0;
-
-    $('#main_menu').animate({ left: '-100%'}, 0);
+    var menu = 1;
+    
     $('#menu_btn').click(function(){
         $('#menu_btn').toggleClass('open');
         if(menu == 0 ){
@@ -9,15 +8,10 @@ $(document).ready(function(){
             menu=1;
         }
         else{
-            $('#main_menu').animate({ left: '-100%'}, 1000);
+            $('#main_menu').animate({ left: '-105%'}, 1000);
             menu=0;
         }
 
     });
-
-    $('#main_menu').click(function(){
-        $('#main_menu').animate({ left: '-100%'}, 1000);
-        menu=0;
-        $('.featured').addClass('hide');
-    });
+   
 });
