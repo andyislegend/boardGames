5b64725305d10b7100c7bf4a57acf631f39d5fa1
VasulBera <vasulbera@gmail.com>
2016-05-20 17:25:40 +0300

    add some changes

--- web/src/main/webapp/resources/js/controllers/notificationController.js
+++ web/src/main/webapp/resources/js/controllers/notificationController.js
@@ -1,21 +1,54 @@
 var app = angular.module('homeApp').controller("notificationCtrl", ['$scope', '$rootScope', '$http', '$interval', function($scope,$rootScope, $http, $interval) {
-	$scope.message = "Hello";
+    
+    $http.get('getCurrentUserName').success(function(data){
+        $scope.currentUserName = data;
+    }).error(function(error){
+        console.log(error);
+    });
+    $scope.arrayOfNotification = [];
+    function Notification(type, emergency, head, body, date){
+        this.type = type;
+        this.emergency = emergency;
+        this.head = head;
+        this.body = body;
+        this.date = date;
+    };
 	$http.get('getAllLastMessage').success(function(data){
         $scope.allLastMessages = data;
+        for(var i = 0 ; i < data.length; i++){
+            var emergency = !data[i].statusOfReading && (data[i].currentUser.username !== $scope.currentUserName);
+            var messageNotification = new Notification('message', emergency, data[i].currentUser.firstName + " " + data[i].currentUser.lastName +  " :", data[i].message, data[i].date);
+            $scope.arrayOfNotification.push(messageNotification);
+        }       
     }).error(function(error){
         console.log(error);
     });
     
     $http.get('getAllCurrentUserTournament').success(function(data){
+        
         $scope.allCurrentUserTournament = data;
-        console.log($scope.allCurrentUserTournament);
+        for(var i = 0 ; i < data.length; i++){
+            var emergency =  (data[i][2] - new Date().getTime()) <= 24*60*60*1000;
+            var tournamentNotification = new Notification('Tournament', emergency, data[i][4] + ", " + data[i][5], data[i][1], data[i][2]);
+            $scope.arrayOfNotification.push(tournamentNotification);
+        }
+        console.log($scope.arrayOfNotification);
     }).error(function(error){
         console.log(error);
     });
+	$scope.specificClassLine = function(emergency, type){
+        if(emergency && type == 'message'){
+            return 'notreadmessage';
+        }else if(emergency && type == 'Tournament'){
+            return 'notification-line-tournament';
+        };
+    };
+    $scope.specificClass = function(emergency, type){
+        if(emergency && type == 'message'){
+            return 'notreadmessage-body';
+        }else if(emergency && type == 'Tournament'){
+            return 'tournament-body';
+        }; 
+    }
 	
-	$http.get('getCurrentUserName').success(function(data){
-        $scope.currentUserName = data;
-    }).error(function(error){
-        console.log(error);
-    });
 }]);
