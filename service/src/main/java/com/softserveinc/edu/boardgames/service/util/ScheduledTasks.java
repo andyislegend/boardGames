9779023e71ccadd4d0cbcb0d6e1bbe651d3a9358
tarasVarvariuk <taras20109@gmail.com>
2016-05-23 16:38:28 +0300

    fixed scheduler

--- service/src/main/java/com/softserveinc/edu/boardgames/service/util/ScheduledTasks.java
+++ service/src/main/java/com/softserveinc/edu/boardgames/service/util/ScheduledTasks.java
@@ -1,16 +1,20 @@
 package com.softserveinc.edu.boardgames.service.util;
 
 import java.util.Calendar;
+import java.util.Date;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.softserveinc.edu.boardgames.persistence.entity.User;
 import com.softserveinc.edu.boardgames.persistence.entity.VerificationToken;
+import com.softserveinc.edu.boardgames.persistence.enumeration.TimeEnum;
 import com.softserveinc.edu.boardgames.service.ExchangeService;
 import com.softserveinc.edu.boardgames.service.MailService;
 import com.softserveinc.edu.boardgames.service.UserService;
+import com.softserveinc.edu.boardgames.persistence.entity.Exchange;
 
 @Component
 public class ScheduledTasks {
@@ -23,61 +27,74 @@ public class ScheduledTasks {
 	
 	@Autowired
 	UserService userService;
-
-//    @Scheduled(fixedRate = 86400000)
-//    public void checkIfOneDayRemains() {
-//    	for (Exchange exchange: exchangeService.findAllExchanges()) {
-//    		
-//    		Date localDate = new Date();
-//    		Date deadLine = exchange.getApplyingDate();
-//    		Calendar calendar = Calendar.getInstance();
-//    		calendar.setTime(deadLine);
-//    		calendar.add(Calendar.DATE, exchange.getPeriod()); 
-//    		deadLine = calendar.getTime();
-//    		Long days = (deadLine.getTime() - localDate.getTime())/ (24 * 60 * 60 * 1000);
-//    		if (days.intValue() == 1) {
-//    			mailService.remindAboutGameReturnTomorrow(
-//    					userService.findById(exchange.getUserApplierId()).getEmail(), 
-//    					userService.findById(exchange.getUserApplierId()).getUsername(),
-//    					exchange.getGameUser().getGame().getName(), 
-//    					exchange.getUser().getUsername());
-//    		}
-//    	}
-//    }
-//    
-//    @Scheduled(fixedRate = 604800000)
-//    public void remindThatNeedsToGiveBack() {
-//    	for (Exchange exchange: exchangeService.findAllExchanges()) {
-//    		Date localDate = new Date();
-//    		Date deadLine = exchange.getApplyingDate();
-//    		Calendar calendar = Calendar.getInstance();
-//    		calendar.setTime(deadLine);
-//    		calendar.add(Calendar.DATE, exchange.getPeriod()); 
-//    		deadLine = calendar.getTime();
-//    		Long days = (deadLine.getTime() - localDate.getTime())/ (24 * 60 * 60 * 1000);
-//    		if (days.intValue() < 1) {
-//    			mailService.remindThatYouAreLate(
-//    					userService.findById(exchange.getUserApplierId()).getEmail(), 
-//    					userService.findById(exchange.getUserApplierId()).getUsername(),
-//    					(-days.intValue()),
-//    					exchange.getGameUser().getGame().getName(), 
-//    					exchange.getUser().getUsername());
-//    		}
-//    	}
-//    }
 	
-//	@Scheduled(fixedRate = 604800000)
-//	public void deleteExpiredTokensAndUsers() {
-//		for (VerificationToken token : userService.findAllTokens()) {
-//
-//			final Calendar calendar = Calendar.getInstance();
-//
-//			if ((token.getExpiryDate().getTime() - calendar.getTime().getTime()) <= 0) {
-//				User userToDelete = userService.getUserByToken(token.getToken());
-//				userService.removeToken(token);
-//				userService.deleteUser(userToDelete);
-//			}
-//		}
-//	}
+	private final Logger logger = Logger.getLogger(MailService.class);
+
+    @Scheduled(cron = "0 33 16 * * *")
+    public void checkIfOneDayRemains() {
+    	for (Exchange exchange: exchangeService.findAllExchanges()) {
+    		
+    		Date localDate = new Date();
+    		Date deadLine = exchange.getApplyingDate();
+    		Calendar calendar = Calendar.getInstance();
+    		calendar.setTime(deadLine);
+    		calendar.add(Calendar.DATE, exchange.getPeriod()); 
+    		deadLine = calendar.getTime();
+    		Long days = (deadLine.getTime() - localDate.getTime())
+    				/ (TimeEnum.HOURS.getValue() * TimeEnum.MINUTES.getValue() 
+    				* TimeEnum.SECONDS.getValue() * TimeEnum.MILISECONDS.getValue());
+    		if (days.intValue() == 1) {
+    			mailService.remindAboutGameReturnTomorrow(
+    					userService.findById(exchange.getUserApplierId()).getEmail(), 
+    					userService.findById(exchange.getUserApplierId()).getUsername(),
+    					exchange.getGameUser().getGame().getName(), 
+    					exchange.getUser().getUsername());
+    			logger.info("Message reminding about exchanges term is out for user " 
+    					+ userService.findById(exchange.getUserApplierId()).getUsername() 
+    					+ " was successfully sent");
+    		}
+    	}
+    	logger.info("16:30 - Checking for users that have one day more to give the game back");
+    }
+    
+    @Scheduled(cron = "0 0 0 1 * *")
+    public void remindThatNeedsToGiveBack() {
+    	for (Exchange exchange: exchangeService.findAllExchanges()) {
+    		Date localDate = new Date();
+    		Date deadLine = exchange.getApplyingDate();
+    		Calendar calendar = Calendar.getInstance();
+    		calendar.setTime(deadLine);
+    		calendar.add(Calendar.DATE, exchange.getPeriod()); 
+    		deadLine = calendar.getTime();
+    		Long days = (deadLine.getTime() - localDate.getTime())
+    				/ (TimeEnum.HOURS.getValue() * TimeEnum.MINUTES.getValue() 
+    				* TimeEnum.SECONDS.getValue() * TimeEnum.MILISECONDS.getValue());
+    		if (days.intValue() < 1) {
+    			mailService.remindThatYouAreLate(
+    					userService.findById(exchange.getUserApplierId()).getEmail(), 
+    					userService.findById(exchange.getUserApplierId()).getUsername(),
+    					(-days.intValue()),
+    					exchange.getGameUser().getGame().getName(), 
+    					exchange.getUser().getUsername());
+    			logger.info("Message reminding that a deadline for giving gam back for " 
+    					+ userService.findById(exchange.getUserApplierId()).getUsername() 
+    					+ " has run out");
+    		}
+    	}
+    }
+	
+	@Scheduled(cron = "0 0 0 1 * *")
+	public void deleteExpiredTokensAndUsers() {
+		for (VerificationToken token : userService.findAllTokens()) {
+
+			final Calendar calendar = Calendar.getInstance();
+
+			if ((token.getExpiryDate().getTime() - calendar.getTime().getTime()) <= 0) {
+				User userToDelete = userService.getUserByToken(token.getToken());
+				userService.removeToken(token);
+				userService.deleteUser(userToDelete);
+			}
+		}
+	}
 
 }
