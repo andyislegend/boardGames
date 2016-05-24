f5590899a0a3c776c98a03498442d5ddb739ffc7
vterlyha <vterlyha@gmail.com>
2016-05-24 20:32:30 +0300

    Refreshing avatar

--- persistence/src/main/java/com/softserveinc/edu/boardgames/persistence/repository/UserRepository.java
+++ persistence/src/main/java/com/softserveinc/edu/boardgames/persistence/repository/UserRepository.java
@@ -88,15 +88,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	public String findUsersGender(@Param("username") String username);
 	
 	@Modifying
-	@Query("Update User u Set u.firstName = :firstName, u.lastName = :lastName where u.username = :username")
-	public void updateUserFirstLastName(@Param("firstName") String firstName, 
-			@Param("lastName") String lastName, @Param("username") String username);
-	
-	@Modifying
-	@Query("Update User u Set u.username = :newUsername where u.username = :username")
-	public void updateUsername(@Param("newUsername") String newUsername, @Param("username") String username);
-	
-	@Modifying
 	@Query("Update User u Set u.userRating = userRating + :addUserRating where u.username = :username")
 	public void updateUserRating(@Param("addUserRating") Integer addUserRating, @Param("username") String username);
 	
