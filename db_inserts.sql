9ded4d0af7f85a7eb221e4f7447ed8bd1daf6c03
Volodymyr95 <krokhmalyuk@live.com>
2016-05-26 02:30:01 +0300

    update sql script

--- db_inserts.sql
+++ db_inserts.sql
@@ -201,8 +201,8 @@ INSERT INTO `boardgames`.`friends` (`status`, `user`, `userId`) VALUES ('2', '4'
 INSERT INTO `boardgames`.`tournament` (`city`, `countOfParticipants`, `country`, `dateOfTournament`, `name`, `game_id`, `userCreator_id`,`isTableGenerated` ) VALUES ('Lviv', '4', 'Ukraine', '2016-05-17 00:00:00', 'Chess Tournamnet', '4', '1',FALSE);
 INSERT INTO `boardgames`.`tournament` (`city`, `countOfParticipants`, `country`, `dateOfTournament`, `name`, `game_id`, `userCreator_id`,`isTableGenerated`) VALUES ('Lviv', '6', 'Ukraine', '2016-05-27 00:00:00', 'Monopoly Championship', '1', '3',FALSE);
 INSERT INTO `boardgames`.`tournament` (`city`, `countOfParticipants`, `country`, `dateOfTournament`, `name`, `game_id`, `userCreator_id`,`isTableGenerated`) VALUES ('Lviv', '4', 'Ukraine', '2016-05-26 00:00:00', 'Scrable Cup', '1', '3',FALSE);
-INSERT INTO `boardgames`.`tournament` (`city`, `countOfParticipants`, `country`, `dateOfTournament`, `name`, `game_id`, `userCreator_id`, `isTableGenerated`) VALUES ('Lviv', '4', 'Ukraine', '2016-05-29 00:00:00', 'Mafia tournament', '3', '3', FALSE);
-INSERT INTO `boardgames`.`tournament` (`city`, `countOfParticipants`, `country`, `dateOfTournament`, `name`, `game_id`, `userCreator_id`, `isTableGenerated`) VALUES ('Lviv', '8', 'Ukraine', '2016-05-29 00:00:00', 'Cup of MONOPOLY', '1', '1', FALSE);
+INSERT INTO `boardgames`.`tournament` (`city`, `countOfParticipants`, `country`, `dateOfTournament`, `name`, `game_id`, `userCreator_id`, `isTableGenerated`) VALUES ('Lviv', '4', 'Ukraine', '2016-05-27 00:00:00', 'Mafia tournament', '3', '3', FALSE);
+INSERT INTO `boardgames`.`tournament` (`city`, `countOfParticipants`, `country`, `dateOfTournament`, `name`, `game_id`, `userCreator_id`, `isTableGenerated`) VALUES ('Lviv', '8', 'Ukraine', '2016-05-27 00:00:00', 'Cup of MONOPOLY', '1', '1', FALSE);
 INSERT INTO `boardgames`.`tournament` (`city`, `countOfParticipants`, `country`, `dateOfTournament`, `name`, `game_id`, `userCreator_id`, `isTableGenerated`) VALUES ('Lviv', '4', 'Ukraine', '2016-05-25 00:00:00', 'Memo Cup', '2', '1', FALSE);
 
 INSERT INTO `boardgames`.`tournament_users` (`Tournament_id`, `users_id`) VALUES ('4', '2');
