424703a8778d9561044b34f343392a637242ddaf
VasulBera <vasulbera@gmail.com>
2016-05-26 00:15:28 +0300

    add sone changes

--- persistence/src/main/java/com/softserveinc/edu/boardgames/persistence/repository/NotificationRepository.java
+++ persistence/src/main/java/com/softserveinc/edu/boardgames/persistence/repository/NotificationRepository.java
@@ -72,7 +72,7 @@ public interface NotificationRepository extends JpaRepository<Notification, Inte
 	 * @return list of Notification
 	 */
 	
-	@Query(value = "SELECT * FROM notification WHERE userId =(SELECT id FROM users WHERE username = ?1) "
-			+ "OR user_sender = (SELECT id FROM users WHERE username = ?1)", nativeQuery = true)
+	@Query(value = "SELECT * FROM notification WHERE (userId =(SELECT id FROM users WHERE username = ?1) "
+			+ "OR user_sender = (SELECT id FROM users WHERE username = ?1)) AND (type = 'MESSAGE' OR type = 'NOTIFICATION' OR type = 'EVENT')", nativeQuery = true)
 	public List<Notification> getAllNotificationByUserName(String userName);
 }
