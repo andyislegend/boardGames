580e1ea01b1438ed48e89f1cc62b6de37b8e65e7
vterlyha <vterlyha@gmail.com>
2016-05-25 23:45:12 +0300

    Some code refactoring

--- service/src/test/java/com/softserveinc/edu/boardgames/service/UserServiceTest.java
+++ service/src/test/java/com/softserveinc/edu/boardgames/service/UserServiceTest.java
@@ -15,13 +15,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
-import static org.springframework.ui.velocity.VelocityEngineUtils.mergeTemplateIntoString;
 
 import com.softserveinc.edu.boardgames.persistence.entity.User;
 import com.softserveinc.edu.boardgames.persistence.repository.UserRepository;
@@ -33,26 +33,30 @@ public class UserServiceTest {
 	UserRepository userRepository;
 	
 	@Mock
-    private JavaMailSender mailSender;
+    JavaMailSender mailSender;
 
 	@InjectMocks
 	UserService userService = new UserService();
 	
 	@InjectMocks
-    MailService mailService = new MailService();
+    MailService mailService;
+
+
 	
 
 	@Test
-	public void testFindAll() {
-		List<User> userList = createListWithCustomUser();
-		when(userRepository.findAll()).thenReturn(userList);
-		List<User> result = userService.findAll();
-		verify(userRepository).findAll();
-		assertEquals(userList, result);
+	public void testFindUser() {
+		User user = new User();
+		String username = "vterlyha";
+		user.setUsername(username);
+		when(userRepository.findByUsername(username)).thenReturn(user);
+		User result = userService.getUser(username);
+		verify(userRepository).findByUsername(username);
+		assertEquals(user, result);
 	}
 
 	@Test
-	public void testFindUserWithNegativeRating() {
+	public void testFindAll() {
 		List<User> userList = createListWithCustomUser();
 		when(userRepository.findAll()).thenReturn(userList);
 		List<User> result = userService.findAll();
@@ -71,25 +75,8 @@ public class UserServiceTest {
 	
 	@Test
 	public void testSendMailToBannedUser() {
-		final VelocityEngine velocityEngine = new VelocityEngine();
-		final String username = "vterlyha";
-		final String to = "vterlyha@gmail.com";
-
-		MimeMessagePreparator preparator = new MimeMessagePreparator() {
-			public void prepare(MimeMessage mimeMessage) throws Exception {
-				MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
-				message.setTo(to);
-				message.setFrom(new InternetAddress("boardGamesExchange@gmail.com", "Board's Game Exchange"));
-				Map<String, Object> templateVariables = new HashMap<>();
-				templateVariables.put("name", username);
-				String body = mergeTemplateIntoString(velocityEngine, "/velocity/templates/userBanTemplate.vm", "UTF-8",
-						templateVariables);
-				message.setText(body, true);
-				message.setSubject("Banning because of negative feedbacks");
-			}
-		};
-		mailSender.send(preparator);
-		Mockito.verify(mailSender).send(preparator);
+		mailService.sendMailToBannedUser("vterlyha", "vterlyha@gmail.com");
+		Mockito.verify(mailSender).send(any(MimeMessagePreparator.class));
 	}
 	
 	private List<User> createListWithCustomUser() {
