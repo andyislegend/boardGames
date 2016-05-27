b25b26ced23daf9798d9ef9b49479ce8fe27f828
vterlyha <vterlyha@gmail.com>
2016-05-27 12:13:40 +0300

    Added userservice interface

--- service/src/test/java/com/softserveinc/edu/boardgames/service/UserServiceTest.java
+++ service/src/test/java/com/softserveinc/edu/boardgames/service/UserServiceTest.java
@@ -1,23 +1,15 @@
 package com.softserveinc.edu.boardgames.service;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
-import org.apache.velocity.app.VelocityEngine;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 
 import static org.junit.Assert.assertEquals;
@@ -25,6 +17,7 @@ import static org.mockito.Mockito.*;
 
 import com.softserveinc.edu.boardgames.persistence.entity.User;
 import com.softserveinc.edu.boardgames.persistence.repository.UserRepository;
+import com.softserveinc.edu.boardgames.service.Impl.UserServiceImpl;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
@@ -36,7 +29,7 @@ public class UserServiceTest {
     JavaMailSender mailSender;
 
 	@InjectMocks
-	UserService userService = new UserService();
+	UserServiceImpl userService = new UserServiceImpl();
 	
 	@InjectMocks
     MailService mailService;
