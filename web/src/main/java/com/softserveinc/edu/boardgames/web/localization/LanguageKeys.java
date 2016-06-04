92b3fdf4ff1ae6d2dd9e9a8902f03074f0313bec
andyislegend <andrew.petryk@gmail.com>
2016-06-04 11:15:05 +0300

    add language keys for registration page

--- web/src/main/java/com/softserveinc/edu/boardgames/web/localization/LanguageKeys.java
+++ web/src/main/java/com/softserveinc/edu/boardgames/web/localization/LanguageKeys.java
@@ -1,6 +1,8 @@
 package com.softserveinc.edu.boardgames.web.localization;
 
 public interface LanguageKeys {
+	
+	// language keys for edit profile page
 	public static final String CHANGES_SAVED = "CHANGES_SAVED";
 	public static final String IMAGE_UPLOAD = "IMAGE_UPLOAD";
 	public static final String IMAGE_UPLOAD_FAILED = "IMAGE_UPLOAD_FAILED";
@@ -9,4 +11,13 @@ public interface LanguageKeys {
 	public static final String OLD_PASSWORD_ANSWER = "OLD_PASSWORD_ANSWER";
 	public static final String NEW_PASSWORD_ANSWER = "NEW_PASSWORD_ANSWER";
 	public static final String CONFIRM_PASSWORD_ANSWER = "CONFIRM_PASSWORD_ANSWER";
+	
+	//language keys for registration page
+	public static final String REQUIRED_FIELD = "REQUIRED_FIELD";
+	public static final String USERNAME_DUPLICATE = "USERNAME_DUPLICATE";
+	public static final String INVALID_USERNAME = "INVALID_USERNAME";
+	public static final String EMAIL_DUPLICATE = "EMAIL_DUPLICATE";
+	public static final String INVALID_EMAIL = "INVALID_EMAIL";
+	public static final String INVALID_PASSWORD = "INVALID_PASSWORD";
+	public static final String NOT_CONFIRMED_PASSWORD = "NOT_CONFIRMED_PASSWORD";
 }
