d45883a7dd0e78793916f54e4aac6e6fa3b759db
andyislegend <andrew.petryk@gmail.com>
2016-06-13 22:04:33 +0300

    add comments to IndexController

--- web/src/main/java/com/softserveinc/edu/boardgames/web/controller/IndexController.java
+++ web/src/main/java/com/softserveinc/edu/boardgames/web/controller/IndexController.java
@@ -21,17 +21,29 @@ public class IndexController {
 	 * Represent path to index.jsp page with login and registration options
 	 */
 	private static final String INDEX_PAGE = "index";
-	
+
 	/**
 	 * Represent path to home.jsp page available after successful log in
 	 */
 	private final static String HOME_PAGE = "home";
-	
+
+	/**
+	 * Method which redirects you to index page
+	 * 
+	 * @return index.jsp
+	 */
 	@RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
 	public final String getIndexPage() {
 		return INDEX_PAGE;
 	}
 
+	/**
+	 * Method which redirects you to home page, also add currently logged user
+	 * to model
+	 * 
+	 * @param model
+	 * @return home.jsp
+	 */
 	@RequestMapping(value = "/home**", method = RequestMethod.GET)
 	public final String getUserPage(Model model) {
 		model.addAttribute("user", WebUtil.getPrincipalUsername());
