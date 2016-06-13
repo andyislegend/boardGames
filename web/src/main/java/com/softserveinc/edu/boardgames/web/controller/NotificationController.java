a70a317aee12a1202c56d2629c8f34dd44262ac0
tarasVarvariuk <taras20109@gmail.com>
2016-06-13 23:54:45 +0300

    added return statments for controllers

--- web/src/main/java/com/softserveinc/edu/boardgames/web/controller/NotificationController.java
+++ web/src/main/java/com/softserveinc/edu/boardgames/web/controller/NotificationController.java
@@ -3,6 +3,8 @@ package com.softserveinc.edu.boardgames.web.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -41,10 +43,11 @@ public class NotificationController {
 	
 	@RequestMapping(value = "/markNotificationAsChecked/{notificationId}", method = RequestMethod.PUT)
 	@ResponseBody
-	public void markAsChecked(@PathVariable Integer notificationId) {
+	public ResponseEntity<String> markAsChecked(@PathVariable Integer notificationId) {
 		Notification notify = notificationService.findById(notificationId);
 		notify.setStatus(NotificationStatus.CHECKED.name());
 		notificationService.update(notify);
+		return new ResponseEntity<String>(notify.toString(), HttpStatus.OK);
 	}
 	
 	@RequestMapping(value = "/getCountOfNotifications", method = RequestMethod.GET)
