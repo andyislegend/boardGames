80109f0e4b84fc244581598c1f84e11213a83598
andyislegend <andrew.petryk@gmail.com>
2016-06-13 22:00:51 +0300

    refractoring IndexController

--- web/src/main/java/com/softserveinc/edu/boardgames/web/controller/IndexController.java
+++ web/src/main/java/com/softserveinc/edu/boardgames/web/controller/IndexController.java
@@ -1,13 +1,10 @@
 package com.softserveinc.edu.boardgames.web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.softserveinc.edu.boardgames.service.UserService;
 import com.softserveinc.edu.boardgames.web.util.WebUtil;
 
 /**
@@ -20,22 +17,26 @@ import com.softserveinc.edu.boardgames.web.util.WebUtil;
 @Controller
 public class IndexController {
 
-	@Autowired
-	UserService userService;
-
-	@Autowired
-	MessageSource messageSource;
-
+	/**
+	 * Represent path to index.jsp page with login and registration options
+	 */
+	private static final String INDEX_PAGE = "index";
+	
+	/**
+	 * Represent path to home.jsp page available after successful log in
+	 */
+	private final static String HOME_PAGE = "home";
+	
 	@RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
 	public final String getIndexPage() {
-		return "index";
+		return INDEX_PAGE;
 	}
 
 	@RequestMapping(value = "/home**", method = RequestMethod.GET)
 	public final String getUserPage(Model model) {
 		model.addAttribute("user", WebUtil.getPrincipalUsername());
 
-		return "home";
+		return HOME_PAGE;
 	}
 
 }
