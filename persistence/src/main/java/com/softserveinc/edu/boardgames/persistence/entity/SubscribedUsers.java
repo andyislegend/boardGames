c05f456dc9b1a180def1ac6d5e5971a34008547d
andyislegend <andrew.petryk@gmail.com>
2016-06-15 10:38:46 +0300

    removed unused code in SubscribedUsers class

--- persistence/src/main/java/com/softserveinc/edu/boardgames/persistence/entity/SubscribedUsers.java
+++ persistence/src/main/java/com/softserveinc/edu/boardgames/persistence/entity/SubscribedUsers.java
@@ -2,10 +2,8 @@ package com.softserveinc.edu.boardgames.persistence.entity;
 
 import java.io.Serializable;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
