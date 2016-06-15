6b6e9f7ada69f617b9e51a70b87355c5fb6cfb63
andyislegend <andrew.petryk@gmail.com>
2016-06-15 12:15:57 +0300

    refractored eventInfo page and delete unused html page and css file

---
+++ web/src/main/webapp/resources/js/googlemap.js
@@ -0,0 +1,5 @@
+var mapCanvas = document.getElementById("eventInfoMap");
+var mapOptions = {
+    center: new google.maps.LatLng(51.5, -0.2), zoom: 10
+}
+var map = new google.maps.Map(mapCanvas, mapOptions);
